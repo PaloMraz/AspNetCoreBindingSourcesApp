c1b8a4b556af7f428557f8993d9b8113c4b4de72
Pavol Mráz <pavol.mraz@fores.group>
2019-07-24 19:00:16 +0200

    Addes PersonController/View/ViewModel to test posting JSON / FORM data.

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,31 @@
+﻿using AspNetCoreBindingSourcesApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreBindingSourcesApp.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+
+        [HttpPost]
+        public IActionResult PostAsJson([FromBody] PersonViewModel model)
+        {            
+            return this.Ok($"{model} (posted as JSON)");
+        }
+
+
+        [HttpPost]
+        public IActionResult PostAsForm(PersonViewModel model)
+        {
+           return this.Ok($"{model} (posted as FORM)");
+        }
+    }
+}
---
+++ Models/PersonViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreBindingSourcesApp.Models
+{
+    public class PersonViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id={Id}, Name={Name}";
+        }
+    }
+}
